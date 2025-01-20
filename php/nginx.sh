ac7af0d9c0277780a8ffc090efeba30f64f81312
CodeExplorerX <codeexplorerx@proton.me>
2025-01-20 05:43:36 +0000

    adding php7.4

--- php/nginx.sh
+++ php/nginx.sh
@@ -10,7 +10,15 @@ curl -o /tmp/nginx_signing.rsa.pub https://nginx.org/keys/nginx_signing.rsa.pub 
 	openssl rsa -pubin -in /tmp/nginx_signing.rsa.pub -text -noout && \
     	mv /tmp/nginx_signing.rsa.pub /etc/apk/keys/
 
-apk add nginx=1.26.2-r1
+ALPINE_VERSION=$(grep '^VERSION_ID=' /etc/os-release | cut -d= -f2 | tr -d '"')
+
+if [[ "$ALPINE_VERSION" == 3.16.* ]]; then
+    NGINX_VERSION="1.26.1-r2"
+else
+    NGINX_VERSION="1.26.2-r1"
+fi
+
+apk add nginx=$NGINX_VERSION
 
 chown -R container:container /srv/start.sh
 chmod +x /srv/start.sh
